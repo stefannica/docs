5d6c94fbe21ddf8e6a8ecc95d3b7fdbb96051f31
FuseML <docs@fuseml>
2021-09-06 14:43:06 +0000

    Deployed 0f6601d to dev with MkDocs 1.2.2 and mike 1.1.0

--- dev/javascripts/extra.js
+++ dev/javascripts/extra.js
@@ -11,10 +11,10 @@ window.addEventListener("DOMContentLoaded", function() {
             }
         }
         if(window.location.pathname.includes("/dev/")) {
-            document.querySelector("div[data-md-component=announce]").innerHTML = "<div id='announce-msg'>You are viewing the FuseML development documentation version. You should <a href='/latest/'>use the latest stable documentation</a> version instead.</div>";
+            document.querySelector("div[data-md-component=announce]").innerHTML = "<div id='announce-msg'>You are viewing the FuseML development documentation version. You should <a href='/docs/latest/'>use the latest stable documentation</a> version instead.</div>";
         }
         else if (!window.location.pathname.includes("/latest/") && (latest_version.length > 0 && !window.location.pathname.includes(latest_version))) {
-            document.querySelector("div[data-md-component=announce]").innerHTML = "<div id='announce-msg'>You are not viewing the documentation for the latest stable FuseML version. You should <a href='/latest/'>use the latest stable documentation</a> version instead.</div>";
+            document.querySelector("div[data-md-component=announce]").innerHTML = "<div id='announce-msg'>You are not viewing the documentation for the latest stable FuseML version. You should <a href='/docs/latest/'>use the latest stable documentation</a> version instead.</div>";
         }
     };
     xhr.send();
